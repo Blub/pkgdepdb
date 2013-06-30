e979d33c6091117ff6f4835f6311990e2cd437c1
Wolfgang Bumiller <wry.git@bumiller.com>
2013-06-30 11:56:47 +0200

    commandline arg handling

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <getopt.h>
 
 #include <memory>
 
@@ -9,6 +11,7 @@
 #include "main.h"
 
 static int LogLevel = Warn;
+static const char *arg0 = 0;
 
 void
 log(int level, const char *msg, ...)
@@ -21,15 +24,64 @@ log(int level, const char *msg, ...)
 	va_end(ap);
 }
 
-int main(int argc, char **argv) {
-	(void)argc; (void)argv;
-	if (argc != 2) {
-		log(Error, "usage error\n");
-		return 1;
+static struct option long_opts[] = {
+	{ "help",    no_argument, 0, 'h' },
+	{ "version", no_argument, 0, 'v' },
+
+	{ 0, 0, 0, 0 }
+};
+
+static void
+help(int x)
+{
+	FILE *out = x ? stderr : stdout;
+	fprintf(out, "usage: %s [options] packages...\n", arg0);
+	fprintf(out, "options:\n"
+	             "  --help       show this message\n"
+	             "  --version    show version info\n");
+	exit(x);
+}
+
+static void
+version(int x)
+{
+	printf("readpkgelf v0.0\n");
+	exit(x);
+}
+
+int
+main(int argc, char **argv)
+{
+	arg0 = argv[0];
+
+	if (argc < 2)
+		help(1);
+
+	for (;;) {
+		int opt_index = 0;
+		int c = getopt_long(argc, argv, "hv", long_opts, &opt_index);
+		if (c == -1)
+			break;
+		switch (c) {
+			case 'h':
+				help(0);
+				break;
+			case 'v':
+				version(0);
+				break;
+			case ':':
+			case '?':
+				help(1);
+				break;
+		}
 	}
 
-	Package package(argv[1]);
-	if (!package)
-		log(Error, "there was an error\n");
+	while (optind < argc) {
+		Package package(argv[optind]);
+		if (!package)
+			log(Error, "error reading package %s\n", argv[optind]);
+		++optind;
+	}
+
 	return 0;
 }
